29389d1de6238a3d5d19ae6f95db2267fad0038c
oceanm7n <aaramanovich@gmail.com>
2019-07-09 14:01:29 +0300

    Public removed from .gitignore

---
+++ client/build/precache-manifest.19d63030659d1af02b6b3a305c80774c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d31d39d841c449ff3eb6705b089a977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81087c1d8b79ca240b18",
+    "url": "/static/css/main.d4ca887e.chunk.css"
+  },
+  {
+    "revision": "d5c7a82d209459df4a4a",
+    "url": "/static/js/2.cff96332.chunk.js"
+  },
+  {
+    "revision": "81087c1d8b79ca240b18",
+    "url": "/static/js/main.91f6bf56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
